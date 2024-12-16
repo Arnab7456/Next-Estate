f02edf8542b564d47e7bb615284813be34b399cf
arnav <arnabdas2753@gmail.com>
2024-12-16 16:25:04 +0530

    retest2

--- src/lib/mongodb/mongoose.ts
+++ src/lib/mongodb/mongoose.ts
@@ -1,22 +1,24 @@
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const connect = async (): Promise<void> => {
   
   mongoose.set('strictQuery', true);
-
   
+  // Check if MongoDB is already connected
   if (mongoose.connection.readyState === 1) {
     console.log('MongoDB already connected');
     return;
   }
-  
+
+  // Fetch the MongoDB URI from environment variables
   const mongoUri = process.env.MONGODB;
   if (!mongoUri) {
     throw new Error('MONGODB is not defined in the environment variables');
   }
 
-  
   try {
+    // Connect to MongoDB with dbName 'next-estate'
     await mongoose.connect(mongoUri, {
       dbName: 'next-estate',
     });
