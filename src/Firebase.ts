faff88e05df171e66347ce786b0962d2d19bde3c
arnav <arnabdas2753@gmail.com>
2024-12-17 21:28:04 +0530

    open id

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   projectId: process.env.projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  storageBucket: "nextapp-954bc.firebasestorage.app",
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
   measurementId: process.env.measurementId
